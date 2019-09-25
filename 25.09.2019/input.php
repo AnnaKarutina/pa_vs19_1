126e0efd7b203b35baf44c23bf59a891277e378d
Anna Karutina <anna.karutina@khk.ee>
2019-09-25 11:28:09 +0300

    Kasutaja sisend - HTML vormide kasutamine

---
+++ 25.09.2019/input.php
@@ -0,0 +1,7 @@
+<form action="input_do.php" method="get">
+    <div>
+        <label for="nimi">Nimi:</label>
+        <input type="text" id="nimi" name="nimi">
+    </div>
+    <input type="submit" value="Saada">
+</form>
---
+++ 25.09.2019/input_do.php
@@ -0,0 +1,7 @@
+<?php
+// muutuja defineerimine ja väärtuse võtmine vormist
+$nimi = $_GET['nimi'];
+// väljastamine
+echo 'Nimi: '.$nimi.'<br>';
+echo '<hr>';
+echo '<a href="input.php">Sisesta andmed uuesti</a>';
