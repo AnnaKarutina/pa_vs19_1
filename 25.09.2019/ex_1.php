37f72beee6a356db5c4a43af6b85c661fe7ff225
Anna Karutina <anna.karutina@khk.ee>
2019-09-25 10:43:51 +0300

    Ülesanne 1 - võimalik lahendus

---
+++ 25.09.2019/ex_1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * eesnimi: Eesnimi
+ * perenimi: Perenimi
+ * email: ees.perenimi@khk.ee
+ * vanus: NR
+ * pikkus (m): N.NR
+ * kaal (kg): NR
+ * */
+echo '<h1>Info minust</h1>';
+echo 'eesnimi: Anna<br>';
+echo 'perenimi: Karutina<br>';
+echo 'email: <a href="mailto:anna.karutina@khk.ee">anna.karutina@khk.ee</a><br>';
+echo 'vanus: 38<br>';
+echo 'pikkus (m): 1.62<br>';
+echo 'kaal (kg): 54<br>';
