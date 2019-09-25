a683f43d4da289a3fe5a196c78cdb410d8633b8a
Anna Karutina <anna.karutina@khk.ee>
2019-09-25 11:33:07 +0300

    Kasutaja sisend - kehamassiindeksi arvutamine

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -3,5 +3,13 @@
         <label for="nimi">Nimi:</label>
         <input type="text" id="nimi" name="nimi">
     </div>
+    <div>
+        <label for="pikkus">Pikkus (m):</label>
+        <input type="number" step="0.01" min="0.0" id="pikkus" name="pikkus">
+    </div>
+    <div>
+        <label for="kaal">Kaal (kg):</label>
+        <input type="number" min="0" id="kaal" name="kaal">
+    </div>
     <input type="submit" value="Saada">
 </form>
--- 25.09.2019/input_do.php
+++ 25.09.2019/input_do.php
@@ -1,7 +1,14 @@
 <?php
 // muutuja defineerimine ja väärtuse võtmine vormist
 $nimi = $_GET['nimi'];
+$pikkus = $_GET['pikkus'];
+$kaal = $_GET['kaal'];
 // väljastamine
 echo 'Nimi: '.$nimi.'<br>';
+echo 'Pikkus: '.$pikkus.' m.<br>';
+echo 'Kaal: '.$kaal.' kg.<br>';
+echo '<br>';
+$kmi = $kaal / ($pikkus * $pikkus);
+echo 'Sinu KMI on '.$kmi;
 echo '<hr>';
 echo '<a href="input.php">Sisesta andmed uuesti</a>';
