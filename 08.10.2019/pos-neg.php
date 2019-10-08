413576ba5db3b646d2fbf8fe30eea7a4e7eb471b
Anna Karutina <anna.karutina@khk.ee>
2019-10-08 09:05:38 +0300

    if-else lause

---
+++ 08.10.2019/pos-neg.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * file name: pos-neg.php;
+ * autor: anna.karutina;
+ * date: 08.10.2019;
+ */
+// define variables
+$number = -25;
+/*var_dump($number < 0);
+echo '<br>';
+// if statement - if number is < than 0
+if($number < 0) {
+    // work if number < than 0
+    echo $number.' on negatiivne<br>';
+}*/
+// control the value of logical statement
+var_dump($number > 0);
+echo '<br>';
+// if statement - if number is > than 0
+if($number > 0) {
+    // work if number > than 0
+    echo $number.' on positiivne<br>';
+} else {
+    echo $number.' on negatiivne<br>';
+}
+// the program is continue
+echo 'programmi lõpp';
