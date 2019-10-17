64d0ba29c140e408ffda7fb94d1953f69ea9fe12
Anna Karutina <anna.karutina@khk.ee>
2019-10-17 10:46:19 +0300

    20 esimese arvudes olevate algarvude summa

--- 17.10.2019/prime-number.php
+++ 17.10.2019/prime-number.php
@@ -4,6 +4,7 @@
  * autor: anna.karutina;
  * date: 17.10.2019;
  */
+$sum = 0;
 for($number = 1; $number <= 20; $number++) {
   if ($number < 2) {
     echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
@@ -16,9 +17,12 @@ for($number = 1; $number <= 20; $number++) {
     // if number and divider is equal - prime number
     if ($number == $divider) {
       echo $number . ' on algarv<br>';
+      $sum = $sum + $number; // $sum += $number;
     } else {
       // otherwise
       echo $number . ' ei ole algarv<br>';
     }
   }
 }
+echo '<hr>';
+echo 'Antud vahemikus olevate algarvude summa on '.$sum;
