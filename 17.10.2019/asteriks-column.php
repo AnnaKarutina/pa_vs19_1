4b2095ba30e2cf51798870b9362a25a9a19a04e0
Anna Karutina <anna.karutina@khk.ee>
2019-10-17 09:01:34 +0300

    tärniveerg koos rea numbriga - for tsükli abil

--- 17.10.2019/asteriks-column.php
+++ 17.10.2019/asteriks-column.php
@@ -4,7 +4,11 @@
  * autor: anna.karutina;
  * date: 17.10.2019;
  */
-for($count = 1; $count <= 5; $count++){
+for($count = 1; $count <= 20; $count++){
+  echo $count.'&nbsp;';
+  if($count < 10) {
+    echo '&nbsp;&nbsp;';
+  }
   echo '*';
   echo '<br>';
 }
