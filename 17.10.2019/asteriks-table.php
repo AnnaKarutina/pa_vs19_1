3bff45d58e0b58126929aae3e8538fa9f76dbc62
Anna Karutina <anna.karutina@khk.ee>
2019-10-17 09:30:27 +0300

    tärnikolmnurk allapoole suunaga koos rea numbriga - sisseehitatud for tsükli kasutamine

--- 17.10.2019/asteriks-table.php
+++ 17.10.2019/asteriks-table.php
@@ -4,12 +4,12 @@
  * autor: anna.karutina;
  * date: 17.10.2019;
  */
-for($row = 1; $row <= 5; $row++){
+for($row = 1; $row < 6; $row++){
   echo $row.'&nbsp;';
   if($row < 10) {
     echo '&nbsp;&nbsp;';
   }
-  for($col = 1; $col <= $row; $col++){
+  for($col = 1; $col <= (6-$row); $col++){
     echo '*';
     echo '&nbsp;';
   }
