2e4372142636b5aa013d41f9d49db75be82f06fe
Anna Karutina <anna.karutina@khk.ee>
2019-10-17 10:40:58 +0300

    algarvu tuvastamine 20 esimesele arvule

--- 17.10.2019/prime-number.php
+++ 17.10.2019/prime-number.php
@@ -4,20 +4,21 @@
  * autor: anna.karutina;
  * date: 17.10.2019;
  */
-$number = 101;
-if($number < 2) {
-  echo 'Antud arv ei kuulu valitud vahemiku sisse <br>';
-} else {
-  $divider = 2; // define divider
-  while ($number % $divider != 0){
-    // while number is not divided by divider with modulo 0
-    $divider++; // get the next divider value
-  }
-  // if number and divider is equal - prime number
-  if($number == $divider) {
-    echo $number.' on algarv<br>';
+for($number = 1; $number <= 20; $number++) {
+  if ($number < 2) {
+    echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
   } else {
-    // otherwise
-    echo $number.' ei ole algarv<br>';
+    $divider = 2; // define divider
+    while ($number % $divider != 0) {
+      // while number is not divided by divider with modulo 0
+      $divider++; // get the next divider value
+    }
+    // if number and divider is equal - prime number
+    if ($number == $divider) {
+      echo $number . ' on algarv<br>';
+    } else {
+      // otherwise
+      echo $number . ' ei ole algarv<br>';
+    }
   }
 }
