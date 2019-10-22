c2fbe96c4a626fff040383cc9ba8d1a15e2ee371
Anna Karutina <anna.karutina@khk.ee>
2019-10-22 11:16:31 +0300

    tabeli keha ja täis tabel funktsioonide abil

--- 22.10.2019/table.php
+++ 22.10.2019/table.php
@@ -1,3 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+    table, th, td {
+      border: 1px #333 solid; border-collapse: collapse; text-align: center;
+    }
+    th, td {
+      width: 50px;
+      padding: 5px;
+    }
+  </style>
+  <title>Table</title>
+</head>
+<body>
+
 <?php
 /**
  * file name: table.php;
@@ -10,7 +30,6 @@
 // additional challenge:
 // header text value output must be realized by for loop
 function tableHeader($val1, $val2, $val3, $val4) {
-    echo '<table border="1">';
       echo '<thead>';
         echo '<tr>';
         for($col = 1; $col < 5; $col++) {
@@ -18,6 +37,32 @@ function tableHeader($val1, $val2, $val3, $val4) {
         }
         echo '</tr>';
       echo '</thead>';
-    echo '</table>';
 }
-tableHeader('see', 'on', 'tabeli', 'päis');
+// define function tableBody
+// function use 2 parameters - table row and column number
+// function output is the table body
+// for data use the random numbers
+function tableBody($rowNumber, $colNumber){
+  echo '<tbody>';
+    for($row = 1; $row <= $rowNumber; $row++){
+      echo '<tr>';
+      for($col = 1; $col <= $colNumber; $col++){
+        echo '<td>';
+        echo rand(10, 99);
+        echo '</td>';
+      }
+      echo '</tr>';
+    }
+  echo '</tbody>';
+}
+// define function table
+// function use 2 parameters - table row and column number
+// function output is the table
+function table($rowNumber, $colNumber) {
+  echo '<table>';
+  tableHeader('see', 'on', 'tabeli', 'päis');
+  tableBody($rowNumber, $colNumber);
+  echo '</table>';
+}
+
+table(5, 4);
