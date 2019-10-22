573a4d621e77ea628fd2ac6ea0623918a24910cb
Anna Karutina <anna.karutina@khk.ee>
2019-10-22 10:50:47 +0300

    algarvu tuvastamine funktsiooni abil koos tulemuse kontrolli tagastamisega

--- 22.10.2019/prime-number-control.php
+++ 22.10.2019/prime-number-control.php
@@ -7,7 +7,7 @@
 // define function
 function isPrimeNumber($number) {
   if ($number < 2) {
-    $result = $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
+    echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
   } else {
     $divider = 2; // define divider
     while ($number % $divider != 0) {
@@ -16,13 +16,24 @@ function isPrimeNumber($number) {
     }
     // if number and divider is equal - prime number
     if ($number == $divider) {
-      $result = $number . ' on algarv<br>';
+      $result = true;
     } else {
       // otherwise
-      $result = $number . ' ei ole algarv<br>';
+      $result = false;
     }
   }
   return $result;
 }
-// use the defined function
-echo isPrimeNumber(rand(0,1000));
+// define function, which use the boolean result of isPrimeNumber and
+// show the text - is prime or is not prime with the number value
+function isPrimeNumberDescription($number, $isPrime) {
+//  if($iisPrime == true) is the same
+  if($isPrime) {
+    echo $number.' on algarv<br>';
+  } else {
+    echo $number.' ei ole algarv<br>';
+  }
+}
+// use the function
+$number = rand(1, 10);
+isPrimeNumberDescription($number, isPrimeNumber($number));
